85c341ff5ffd61f425102a4761f0ca4533d5c1fa
Anton Drelin <antondrelin@gmail.com>
2022-11-30 16:46:10 +0300

    template

---
+++ offer-filters.ts
@@ -0,0 +1,144 @@
+import { ViewColumn, ViewEntity } from 'typeorm';
+
+@ViewEntity({
+  expression: ` SELECT DISTINCT offer.id AS offer_id,
+    offer.status AS offer_status,
+    offer.type AS offer_type,
+    offer.network AS offer_network,
+    offer.price AS offer_price,
+    offer.currency AS offer_currency,
+    offer.address_from AS offer_address_from,
+    offer.address_to AS offer_address_to,
+    offer.block_number_ask AS offer_block_number_ask,
+    offer.block_number_cancel AS offer_block_number_cancel,
+    offer.block_number_buy AS offer_block_number_buy,
+    offer.created_at_ask AS offer_created_at_ask,
+    offer.updated_at AS offer_updated_at,
+    offer.id AS auction_id,
+    offer.created_at_ask AS auction_created_at,
+    offer.updated_auction AS auction_updated_at,
+    offer.price_step AS auction_price_step,
+    offer.start_price AS auction_start_price,
+    offer.status AS auction_status,
+    offer.stop_at AS auction_stop_at,
+    bid.bidder_address AS auction_bidder_address,
+    search_filter.collection_id,
+    search_filter.network,
+    search_filter.token_id,
+    search_filter.is_trait,
+    search_filter.locale,
+    search_filter.traits,
+    search_filter.key,
+    search_filter.count_item,
+    search_filter.total_items,
+    search_filter.list_items
+   FROM offers offer
+     LEFT JOIN ( SELECT sf.collection_id,
+            sf.network,
+            sf.token_id,
+            sf.is_trait,
+            sf.locale,
+            unnest(sf.items) AS traits,
+            sf.key,
+            sf.count_item,
+            sf.total_items,
+            sf.list_items
+           FROM search_index sf
+          WHERE sf.type <> 'ImageURL'::search_index_type_enum) search_filter ON offer.network::text = search_filter.network::text AND offer.collection_id = search_filter.collection_id AND offer.token_id = search_filter.token_id
+     LEFT JOIN auction_bids bid ON bid.auction_id = offer.id`,
+  name: 'v_offers_search',
+})
+export class OfferFilters {
+  @ViewColumn({ name: 'offer_id' })
+  offerId: string;
+
+  @ViewColumn({ name: 'offer_status' })
+  offerStatus: string; // active, canceled, bought
+
+  @ViewColumn({ name: 'offer_type' })
+  offerType: string; // Fixed, Auction
+
+  @ViewColumn({ name: 'offer_network' })
+  offerNetwork: string;
+
+  @ViewColumn({ name: 'offer_price' })
+  offerPrice: string;
+
+  @ViewColumn({ name: 'offer_currency' })
+  offerCurrency: string;
+
+  @ViewColumn({ name: 'offer_address_from' })
+  offerAddressFrom: string; // address from
+
+  @ViewColumn({ name: 'offer_address_to' })
+  offerAddressTo: string; // address to
+
+  @ViewColumn({ name: 'offer_block_number_ask' })
+  offerBlockNumberAsk: string;
+
+  @ViewColumn({ name: 'offer_block_number_cancel' })
+  offerBlockNumberCancel: string;
+
+  @ViewColumn({ name: 'offer_block_number_buy' })
+  offerBlockNumberBuy: string;
+
+  @ViewColumn({ name: 'offer_created_at_ask' })
+  offerCreatedAtAsk: Date;
+
+  @ViewColumn({ name: 'offer_updated_at' })
+  offerUpdatedAt: Date;
+
+  @ViewColumn({ name: 'auction_id' })
+  auctionId: string;
+
+  @ViewColumn({ name: 'auction_created_at' })
+  auctionCreatedAt: Date;
+
+  @ViewColumn({ name: 'auction_updated_at' })
+  auctionUpdatedAt: Date;
+
+  @ViewColumn({ name: 'auction_price_step' })
+  auctionPriceStep: string;
+
+  @ViewColumn({ name: 'auction_start_price' })
+  auctionStartPrice: string;
+
+  @ViewColumn({ name: 'auction_status' })
+  auctionStatus: string; // created, active, stopped, withdrawing, ended
+
+  @ViewColumn({ name: 'auction_stop_at' })
+  auctionStopAt: Date;
+
+  @ViewColumn({ name: 'auction_bidder_address' })
+  auctionBidderAddress: string;
+
+  @ViewColumn({ name: 'collection_id' })
+  collectionId: number; // collection id from search_index
+
+  @ViewColumn({ name: 'network' })
+  network: string;
+
+  @ViewColumn({ name: 'token_id' })
+  tokenId: number; // token id from search_index
+
+  @ViewColumn({ name: 'is_trait' })
+  isTrait: boolean;
+
+  @ViewColumn({ name: 'locale' })
+  locale: string;
+
+  @ViewColumn({ name: 'traits' })
+  traits: string;
+
+  @ViewColumn({ name: 'key' })
+  key: string;
+
+  @ViewColumn({ name: 'count_item' })
+  countItem: number;
+
+  @ViewColumn({ name: 'total_items' })
+  totalItems: number;
+
+  @ViewColumn({ name: 'list_items' })
+  listItems: string[];
+}
---
+++ search-index.service.ts
@@ -0,0 +1,297 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { MarketConfig } from '@app/config';
+import { Collection, SearchIndex } from '@app/entity';
+
+import { v4 as uuid } from 'uuid';
+import { CollectionToken, TokenInfo, TypeAttributToken } from '@app/types';
+
+import '@polkadot/api-augment/polkadot';
+import { CollectionInfoWithSchema, NestedToken, TokenByIdResult } from '@unique-nft/substrate-client/tokens';
+
+import { TokenService } from '@app/database/token.service';
+import { CollectionService } from '@app/database/collection.service';
+import { InjectUniqueSDK, SdkProvider } from '@app/uniquesdk';
+
+import { BundleType } from '@app/uniquesdk/sdk-tokens.service';
+import { SearchIndexDBService } from '@app/database/search-index.service';
+
+type SerializeTokenType = {
+  items: TokenInfo[];
+  token: TokenByIdResult;
+  collection: CollectionInfoWithSchema;
+  serializeBunlde: Array<BundleType>;
+};
+
+type AddTokenType = {
+  collectionId: number;
+  tokenId: number;
+  owner: string;
+  data: string;
+};
+
+type TokenType = Omit<TokenByIdResult, 'nestingChildTokens'> & {
+  nestingChildTokens?: NestedToken & { collection: any }[];
+};
+
+@Injectable()
+export class SearchIndexService {
+  private network: string;
+  private repository: Repository<SearchIndex>;
+  private readonly collectionsRepository: Repository<Collection>;
+  private readonly logger = new Logger(SearchIndexService.name);
+
+  private BLOCKED_SCHEMA_KEYS = ['ipfsJson'];
+
+  constructor(
+    private connection: DataSource,
+    @InjectUniqueSDK() private readonly uniqueProvider: SdkProvider,
+    private tokenDB: TokenService,
+    private collectionDB: CollectionService,
+    private searchIndexDB: SearchIndexDBService,
+    @Inject('CONFIG') private config: MarketConfig,
+  ) {
+    this.network = this.config.blockchain.unique.network;
+    this.repository = connection.getRepository(SearchIndex);
+  }
+  /**
+   * If token is not exist in database, get token from unique api and save to database
+   * @param {CollectionToken} collectionToken token in collection
+   * @returns {Promise<SearchIndex[]>} Array of token info
+   */
+  async addSearchIndexIfNotExists(collectionToken: CollectionToken): Promise<SearchIndex[]> {
+    const dbIndex = await this.searchIndexDB.findByTokenId(
+      String(collectionToken.tokenId),
+      String(collectionToken.collectionId),
+      collectionToken?.network || this.network,
+    );
+    if (dbIndex.length) {
+      await this.searchIndexDB.deleteByTokenId(String(collectionToken.tokenId), String(collectionToken.collectionId));
+    }
+    const searchIndexItems = await this.getTokenInfoItems(collectionToken);
+    return this.saveSearchIndex(collectionToken, searchIndexItems);
+  }
+  /**
+   * Get value from attribute of token
+   * @param attribute
+   * @returns {string} Array of value
+   */
+  private getValueToken(attribute: any): Array<string> {
+    if (Array.isArray(attribute.value)) {
+      return attribute.value.map((item) => item._);
+    }
+    return attribute.value._ ? [attribute.value._] : [attribute.value];
+  }
+  /**
+   * Get location from attribute of token
+   * @param attribute
+   * @returns {string}
+   */
+  private getLocation(attribute: any): string | null {
+    if (Array.isArray(attribute.value)) {
+      return [
+        ...new Set(
+          attribute.value.map((i) =>
+            Object.keys(i)
+              .filter((i) => i !== '_')
+              .join(','),
+          ),
+        ),
+      ].join(',');
+    } else {
+      if (typeof attribute.value === 'string') {
+        return null;
+      } else {
+        return Object.keys(attribute.value)
+          .filter((i) => i !== '_')
+          .join(',');
+      }
+    }
+  }
+
+  /**
+   * Save token to database
+   * @param {TokenByIdResult} token token from sdk
+   * @returns {void}
+   */
+  async serializeTokenSave(token: TokenByIdResult, nested: Array<BundleType>): Promise<boolean> {
+    return this.tokenDB.save({
+      collectionId: token.collectionId,
+      tokenId: token.tokenId,
+      owner: token.owner,
+      data: token as any,
+      nested: nested as any,
+    });
+  }
+
+  async prepareSearchIndex(tokenId: number, collectionId: number): Promise<any> {
+    const tokenData = await this.uniqueProvider.tokenWithCollection(tokenId, collectionId);
+    const source = new Set();
+    // Collection
+    source
+      .add({
+        locale: null,
+        items: [tokenData.collection.tokenPrefix],
+        key: 'prefix',
+        type: TypeAttributToken.Prefix,
+        is_trait: false,
+      })
+      .add({
+        locale: null,
+        items: [tokenData.collection.description],
+        key: 'description',
+        type: TypeAttributToken.String,
+        is_trait: false,
+      })
+      .add({
+        locale: null,
+        items: [tokenData.collection.name],
+        key: 'collectionName',
+        type: TypeAttributToken.String,
+        is_trait: false,
+      })
+      .add({
+        locale: null,
+        items: [tokenData.collection?.schema?.coverPicture?.fullUrl],
+        key: 'collectionCover',
+        type: TypeAttributToken.ImageURL,
+        is_trait: false,
+      });
+
+    // Token
+    source.add({
+      locale: null,
+      items: [`${tokenId}`],
+      key: 'tokenId',
+      type: TypeAttributToken.Number,
+      is_trait: false,
+    });
+
+    if (tokenData.token?.image) {
+      source.add({
+        locale: null,
+        items: [tokenData.token.image?.fullUrl],
+        key: 'image',
+        type: TypeAttributToken.ImageURL, //
+        is_trait: false,
+      });
+    }
+
+    if (tokenData.token?.video) {
+      source.add({
+        locale: null,
+        items: [tokenData.token.video?.fullUrl],
+        key: 'video',
+        type: TypeAttributToken.VideoURL,
+      });
+    }
+
+    for (const [, object] of Object.entries(tokenData.token?.attributes)) {
+      source.add({
+        locale: this.getLocation(object),
+        items: this.getValueToken(object),
+        key: object.name?._,
+        type: object.isEnum ? TypeAttributToken.Enum : TypeAttributToken.String,
+        is_trait: object.isEnum,
+      });
+    }
+
+    return {
+      items: [...source],
+      token: tokenData.token,
+      collection: tokenData.collection,
+      serializeBunlde: tokenData.serializeBunlde,
+    };
+  }
+
+  async getTokenInfoItems({ collectionId, tokenId }: CollectionToken): Promise<SerializeTokenType> {
+    const source = await this.prepareSearchIndex(tokenId, collectionId);
+    return source;
+  }
+
+  private dropProperties(token: TokenType): TokenByIdResult {
+    const properties = token?.properties || [];
+    if (properties.length > 0) {
+      delete token.properties;
+    }
+    if (token?.nestingChildTokens) {
+      token.nestingChildTokens.forEach((item) => {
+        this.dropProperties(item as TokenType);
+      });
+    }
+    return token;
+  }
+
+  async saveSearchIndex(collectionToken: CollectionToken, source: SerializeTokenType): Promise<SearchIndex[]> {
+    const items = source.items;
+    const total = items
+      .filter(
+        (i) =>
+          [TypeAttributToken.Enum, TypeAttributToken.String].includes(i.type) &&
+          !['collectionCover', 'prefix', 'description', 'collectionName', 'tokenId', 'image', 'video'].includes(i.key),
+      )
+      .reduce((acc, item) => {
+        return acc + item.items.length;
+      }, 0);
+    const listItems = this.setListItems(items);
+    source.token = this.dropProperties(source.token);
+
+    await this.serializeTokenSave(source.token, source.serializeBunlde);
+    await this.collectionDB.save(source.collection);
+
+    const searchIndexItems: SearchIndex[] = items.map((item) =>
+      this.repository.create({
+        id: uuid(),
+        collection_id: String(collectionToken.collectionId),
+        token_id: String(collectionToken.tokenId),
+        network: collectionToken?.network || this.network,
+        locale: item.locale,
+        items: item.items,
+        key: item.key,
+        is_trait: item.is_trait,
+        type: item.type,
+        count_item: this.setCountItem(item),
+        total_items: total,
+        list_items: listItems,
+        attributes: source.token as any,
+        nested: source.serializeBunlde as any,
+      }),
+    );
+
+    return this.repository.save(searchIndexItems);
+  }
+
+  private setCountItem(item: TokenInfo): number {
+    if (
+      [TypeAttributToken.Enum, TypeAttributToken.String].includes(item.type) &&
+      !['collectionCover', 'prefix', 'description', 'collectionName'].includes(item.key)
+    ) {
+      return 0;
+    }
+    return item.items.length;
+  }
+
+  private setListItems(items: TokenInfo[]): string[] {
+    return items
+      .filter(
+        (i) =>
+          [TypeAttributToken.Enum, TypeAttributToken.String].includes(i.type) &&
+          !['collectionCover', 'prefix', 'description', 'collectionName', 'tokenId', 'image'].includes(i.key),
+      )
+      .reduce((acc, item) => {
+        acc = [...acc, ...item.items];
+        return acc;
+      }, []);
+  }
+
+  async updateSearchIndex(): Promise<void> {
+    for (const index of await this.repository.query(`select collection_id, token_id from search_index group by collection_id, token_id`)) {
+      await this.searchIndexDB.deleteByTokenId(index.token_id, index.collection_id);
+
+      await this.addSearchIndexIfNotExists({
+        collectionId: index?.collection_id,
+        tokenId: index?.token_id,
+      });
+    }
+  }
+}
